dfc7b7debf1faaf69f0788f2ac623952c1476cba
Luca Santos <santoslucas8599@gmail.com>
2021-02-11 23:33:02 -0300

    Starting to add style to the tutorial project

--- mysite/settings.py
+++ mysite/settings.py
@@ -35,9 +35,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # libs
+    'crispy_forms',
     #apps
-    'polls'
-    #libs
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
